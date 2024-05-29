34b2214bd90a2f9a00cb7d0ab28bcff5979b6e4b
AndreyFan <130384653+AndreyFan@users.noreply.github.com>
2024-05-29 22:26:59 +0200

    hw2_22mai

---
+++ hw2_22_05_2024/Concatenation.java
@@ -0,0 +1,11 @@
+package org.example.hw2_22_05_2024;
+
+public class Concatenation {
+    public static void main(String[] args) {
+        String str1 = "Good ";
+        String str2 = "day";
+        System.out.println(str1 + str2);
+
+
+    }
+}
